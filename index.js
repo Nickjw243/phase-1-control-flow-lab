a408a4bdc4c611877a2b2872fd2d92eda6feee7f
Nick Weiland <njweiland23@gmail.com>
2023-10-30 20:43:50 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(depth){
   // Write your code here!
+  let payment
+  if (depth <= 400) {
+    payment = 'This one is on me!'
+  } else if (depth > 400 && depth <= 2000) {
+    payment = 'That will be twenty bucks.'
+  } else if (depth > 2000 && depth <= 2500) {
+    payment = 'I will gladly take your thirty bucks.'
+  } else if (depth > 2500) {
+    payment = 'No can do.'
+  }
+  return payment
 }
+scuberGreetingForFeet(199, 1500, 2001, 2501)
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let message = (city === "NYC") ? "Ok, sounds good." : "No go.";
+  return message
 }
+ternaryCheckCity("NYC", "Pittsburgh")
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+  }
+}
+switchOnCharmFromTip(tip)
